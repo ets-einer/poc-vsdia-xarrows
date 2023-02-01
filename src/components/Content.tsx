2a5b2ddb624e349e7148df5a309d59f087531abd
vinicius-prates <viniprates2211@gmail.com>
2023-02-01 09:51:37 -0300

    adding arrow on click, arrow follwing the box while moving
    Co-authored-by: viniciusventura29 <viniciusventura29@gmail.com>

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,18 +1,38 @@
-import React, {useRef} from "react";
-import Xarrow from "react-xarrows";
+import React, {useRef, useState} from "react";
+import Xarrow, { useXarrow } from "react-xarrows";
+import Draggable, { DraggableCore } from "react-draggable";
+
+const AddArrow =(showArrow:any, box1Id:string)=>{
+    return(
+        <div className={`${showArrow? 'visible': 'invisible'}`}>
+        <Xarrow
+                start="fodase" //can be react ref
+                end="elem2" //or an id
+                headSize={3}
+            />
+            </div>
+    )
+}
 
 export const Content = () => {
 
-    const box1Ref = useRef(null);
+    const [showArrow, setShowArrow] = useState(false)
+
+    const box1Id = "fodase";
+    const updateArrow = useXarrow()
+
     return(
         <div>
             <div className="flex  justify-evenly">
-            <div ref={box1Ref} className="border-gray-500 border-2 rounded-sm p-1">hey</div>
+
+            <Draggable onDrag={updateArrow}>
+                <div id={box1Id} className="border-gray-500 border-2 rounded-sm p-1" onClick={()=>setShowArrow(true)}>hey</div>
+                </Draggable>
+                <AddArrow showArrow={showArrow} box1Id={box1Id} />
             <p id="elem2" className="border-gray-500 border-2 rounded-sm p-1">hey2</p>
-            <Xarrow
-                start={box1Ref} //can be react ref
-                end="elem2" //or an id
-            />
+            
+           
+       
         </div>
         </div>
     )
