fa1d74e8fe2520bd15bbf5b3ad26458ee99c8d40
vinicius-prates <viniprates2211@gmail.com>
2023-02-01 09:03:11 -0300

    added react-xarrows

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import { Content } from './components/Content'
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Xarrows POC</h1>
+      <h1 className='font-bold text-xl'>Xarrows POC</h1>
+      <Content/>
     </div>
   )
 }
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,7 +1,19 @@
+import React, {useRef} from "react";
+import Xarrow from "react-xarrows";
+
 export const Content = () => {
+
+    const box1Ref = useRef(null);
     return(
         <div>
-            
+            <div className="flex  justify-evenly">
+            <div ref={box1Ref} className="border-gray-500 border-2 rounded-sm p-1">hey</div>
+            <p id="elem2" className="border-gray-500 border-2 rounded-sm p-1">hey2</p>
+            <Xarrow
+                start={box1Ref} //can be react ref
+                end="elem2" //or an id
+            />
+        </div>
         </div>
     )
 }
